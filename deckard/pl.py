74d39180601b01fbed0ba49646e4cbace584bc28
SpaceShaman <spaceshaman@tuta.io>
2025-08-11 20:01:57 +0200

    ♻️ (pl.py): refactor regex patterns for street and zip code for reusability and clarity

--- deckard/pl.py
+++ deckard/pl.py
@@ -2,8 +2,15 @@ EMAIL = r"(?P<email>\w+@\w+\.\w+)"
 
 MOBILE_PHONE = r"(?P<mobile_phone>\d{3}\s?\d{3}\s?\d{3})"
 
-STREET = r"(?P<street>(ul\.|al\.|pl\.|os\.|ulica|aleja|plac)\s+[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)"
 
-ZIP_CODE = r"(?P<zip_code>\d{2}-\d{3})"
+_STREET_PATTERN = (
+    r"(ul\.|al\.|pl\.|os\.|ulica|aleja|plac)\s+[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+"
+)
 
-CITY = r"(?<=\b\d{2}-\d{3}\s)(?P<city>[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)"
+_ZIP_CODE_PATTERN = r"\d{2}-\d{3}"
+
+STREET = rf"(?P<street>{_STREET_PATTERN})"
+
+ZIP_CODE = rf"(?P<zip_code>{_ZIP_CODE_PATTERN})"
+
+CITY = rf"(?<=\b{_ZIP_CODE_PATTERN}\s)(?P<city>[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)"
