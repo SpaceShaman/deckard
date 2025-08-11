e41788c583a39c0cbee2d477ea0bebe7657a09d6
SpaceShaman <spaceshaman@tuta.io>
2025-08-11 20:14:20 +0200

    ♻️ (pl.py): rename _HOUSE_NO to _HOUSE_NO_PATTERN for consistency

--- deckard/pl.py
+++ deckard/pl.py
@@ -2,10 +2,9 @@ EMAIL = r"(?P<email>\w+@\w+\.\w+)"
 
 MOBILE_PHONE = r"(?P<mobile_phone>\d{3}\s?\d{3}\s?\d{3})"
 
-
 _STREET_PATTERN = r"(?:ul\.|al\.|pl\.|os\.|ulica|aleja|plac)\s+[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(?:[\s-][A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż]+)*"
 
-_HOUSE_NO = r"\d+[A-Za-z]?(?:/\d+[A-Za-z]?)?"
+_HOUSE_NO_PATTERN = r"\d+[A-Za-z]?(?:/\d+[A-Za-z]?)?"
 
 _ZIP_CODE_PATTERN = r"\d{2}-\d{3}"
 
@@ -15,4 +14,4 @@ ZIP_CODE = rf"(?P<zip_code>{_ZIP_CODE_PATTERN})"
 
 CITY = rf"(?<=\b{_ZIP_CODE_PATTERN}\s)(?P<city>[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+)"
 
-STREET_NUMBER = rf"(?:{_STREET_PATTERN})\s+(?P<street_number>{_HOUSE_NO})"
+STREET_NUMBER = rf"(?:{_STREET_PATTERN})\s+(?P<street_number>{_HOUSE_NO_PATTERN})"
