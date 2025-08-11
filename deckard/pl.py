a46dbbc150bd0a009e82361ffcaaf5b33c75eff4
SpaceShaman <spaceshaman@tuta.io>
2025-08-11 20:13:22 +0200

    💡 (pl.py): add a newline for better readability between regex patterns

--- deckard/pl.py
+++ deckard/pl.py
@@ -4,6 +4,7 @@ MOBILE_PHONE = r"(?P<mobile_phone>\d{3}\s?\d{3}\s?\d{3})"
 
 
 _STREET_PATTERN = r"(?:ul\.|al\.|pl\.|os\.|ulica|aleja|plac)\s+[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(?:[\s-][A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż]+)*"
+
 _HOUSE_NO = r"\d+[A-Za-z]?(?:/\d+[A-Za-z]?)?"
 
 _ZIP_CODE_PATTERN = r"\d{2}-\d{3}"
