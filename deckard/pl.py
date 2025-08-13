d8091bb6e3ad21c8ffc0e46b1e45666ab30b7fa2
SpaceShaman <spaceshaman@tuta.io>
2025-08-13 15:57:59 +0200

    ♻️ (pl.py): refactor regex patterns for address components to improve readability and maintainability

--- deckard/pl.py
+++ deckard/pl.py
@@ -2,32 +2,28 @@ EMAIL = r"(?P<email>\w+@\w+\.\w+)"
 
 MOBILE_PHONE = r"(?P<mobile_phone>\d{3}\s?\d{3}\s?\d{3})"
 
-_STREET = r"(?:ul\.|al\.|pl\.|os\.|ulica|aleja|plac)\s+[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(?:[\s-][A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż]+)*"
+_STREET = r"(?P<street>(?:ul\.|al\.|pl\.|os\.|ulica|aleja|plac)\s+[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(?:[\s-][A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż]+)*)"
 
-_HOUSE_NO = r"\d+[A-Za-z]?(?:/\d+[A-Za-z]?)?"
-
-_ZIP_CODE = r"\d{2}-\d{3}"
-
-STREET = rf"(?P<street>{_STREET})"
-
-ZIP_CODE = rf"(?P<zip_code>{_ZIP_CODE})"
+_ZIP_CODE = r"(?P<zip_code>\d{2}-\d{3})"
 
 _WORD = r"[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+(?:-[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż]+)*"
 _CONNECT = r"(?:nad|pod|przy|w|we|koło)"
 _TOKEN = rf"(?:{_WORD}|{_CONNECT})"
 
-STREET_NUMBER = rf"(?:{STREET})\s+(?P<street_number>{_HOUSE_NO})"
+_BUILDING = r"(?P<building>\d+[A-Za-z]?(?:/\d+[A-Za-z]?)?)"
 
-CITY_0 = rf"(?<=\b{_ZIP_CODE}\s)(?P<city>{_TOKEN}(?:[ -]{_TOKEN}){{0,4}})"
+_CITY = rf"(?P<city>{_TOKEN}(?:[ -]{_TOKEN}){{0,4}})"
 
-CITY_1 = rf"(?P<city>{_TOKEN}(?:[ -]{_TOKEN}){{0,4}})(?=\s+{_HOUSE_NO}\b)(?=.*?\b{_ZIP_CODE}\b)"
+_CITY_0 = rf"(?<=\b{_ZIP_CODE}\s){_CITY}"
+
+_CITY_1 = rf"{_CITY}(?=\s+{_BUILDING}\b)(?=.*?\b{_ZIP_CODE}\b)"
 
 _ADDRESS_0 = (
-    rf"(?s)^(?=.*?{STREET})(?=.*?{STREET_NUMBER})(?=.*?{ZIP_CODE})(?=.*?{CITY_0})"
+    rf"(?s)^(?=.*?{_STREET})(?=.*?{_BUILDING})(?=.*?{_ZIP_CODE})(?=.*?{_CITY_0})"
 )
 
 _ADDRESS_1 = (
-    rf"(?s)^(?=.*?{CITY_1})(?=.*?\b(?P<street_number>{_HOUSE_NO})\b)(?=.*?{ZIP_CODE})"
+    rf"(?s)^(?=.*?{_CITY_1})(?=.*?\b(?P<building>{_BUILDING})\b)(?=.*?{_ZIP_CODE})"
 )
 
 ADDRESS = rf"(?x)(?|{_ADDRESS_0}|{_ADDRESS_1})"
